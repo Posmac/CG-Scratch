88794b48555802c4327b2e90473fb260e10f2a88
Posmac <nicolai.posmac@gmail.com>
2021-04-08 16:50:06 +0300

    Added link directories

--- Headers/math.h
+++ Headers/math.h
@@ -6,14 +6,13 @@
 namespace MATH
 {
     template<typename T>
-    class Vector3<T>
+    class Vector3
     {
     public:
         T x, y, z;
         Vector3 () : x(0), y(0), z(0) {}
         Vector3 (T v) : x(v), y(v), z(v) {}
         Vector3 (T v1, T v2, T v3) : x(v1), y(v2), z(v3) {}
-
     };
 
     typedef Vector3<float> Vector3f;
--- SourceFiles/main.cpp
+++ SourceFiles/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <math.h>
-
-
-using namespace std;
+#include "math.h"
 
 int main()
 {
+    MATH::Vector3f vec(0.0f);
     return 0;
 }
