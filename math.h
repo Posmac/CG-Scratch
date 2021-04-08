4ee95166421851a2278fd526600db46544b6266a
Posmac <nicolai.posmac@gmail.com>
2021-04-08 16:35:06 +0300

    First commit

---
+++ math.h
@@ -0,0 +1,22 @@
+#ifndef CUSTOM_MATH
+#define CUSTOM_MATH
+
+#include <cmath>
+
+namespace MATH
+{
+    template<typename T>
+    class Vector3<T>
+    {
+    public:
+        T x, y, z;
+        Vector3 () : x(0), y(0), z(0) {}
+        Vector3 (T v) : x(v), y(v), z(v) {}
+        Vector3 (T v1, T v2, T v3) : x(v1), y(v2), z(v3) {}
+
+    };
+
+    typedef Vector3<float> Vector3f;
+}
+#endif // CUSTOM_MATH
+
